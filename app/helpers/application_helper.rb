48df15ee3e24628812d45f529a845b802a49544c
Taiwo Kareem <taiwo.kareem36@gmail.com>
2020-02-29 01:51:03 +0000

    added page helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # returns the full title on a per-page basis
+  def full_title(page_title = '')
+    base_title = 'ToyApp'
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
